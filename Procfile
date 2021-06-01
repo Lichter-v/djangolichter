8569c2a664d94bd0231b2a23c6c3f5e80739abf3
lichter <tatiha84@gmail.com>
2021-05-31 21:50:45 -0300

    Configurações do heroku implementadas.

---
+++ Procfile
@@ -0,0 +1,3 @@
+release: python3 manage.py migrate
+
+web: gunicorn lichter.wsgi --preload --log-file -
--- lichter/asgi.py
+++ lichter/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lichter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lichter.settings.base')
 
 application = get_asgi_application()
